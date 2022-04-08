855e528bbc171ceec508e52da62c7eeb8d4a6b34
sia162 <nowitissiya@gmail.com>
2022-04-09 01:22:00 +0530

    b9: using hooks package from npm and its useDimension and useOrientation

--- App.js
+++ App.js
@@ -7,16 +7,23 @@ import {
   Dimensions,
 } from "react-native";
 
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+
 export default function App() {
-  console.log(Dimensions.get("screen"));
+  // console.log(useDimensions());
+  // console.log(useDeviceOrientation());
+  const { landscape } = useDeviceOrientation();
 
   return (
     <SafeAreaView style={styles.container}>
       <View
         style={{
           backgroundColor: "green",
-          width: "50%",
-          height: 70,
+          width: "100%",
+          height: landscape ? "100%" : "30%",
         }}
       ></View>
       <StatusBar style="auto" />

31ac4c9d76b09124245eb76e4605cc5d023d1e7b
sia162 <nowitissiya@gmail.com>
2022-04-08 23:20:13 +0530

    b3: TouchableWithoutFeedback

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, SafeAreaView, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello Image!</Text>
-      <Image
-        blurRadius={10}
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <TouchableWithoutFeedback onPress={() => console.log("image pressed")}>
+        <Image
+          blurRadius={10}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </SafeAreaView>
   );

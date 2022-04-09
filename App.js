c692c15f6544ae61d214ee22bdb536279b3b6715
sia162 <nowitissiya@gmail.com>
2022-04-09 14:49:09 +0530

    b10: Align items and align content

--- App.js
+++ App.js
@@ -5,26 +5,32 @@ export default function App() {
   return (
     <View
       style={{
-        backgroundColor: "grey",
+        backgroundColor: "#fff",
         flex: 1,
         flexDirection: "row",
-        justifyContent: "space-around",
+        justifyContent: "center",
         alignItems: "center",
+        flexWrap: "wrap",
+        alignContent: "center",
       }}
     >
       <View
         style={{ backgroundColor: "orange", width: 100, height: 100 }}
       ></View>
-      <View
-        style={{
-          backgroundColor: "red",
-          width: 100,
-          height: 200,
-          alignSelf: "flex-end",
-        }}
-      ></View>
+      <View style={{ backgroundColor: "red", width: 100, height: 100 }}></View>
       <View
         style={{ backgroundColor: "green", width: 100, height: 100 }}
+      ></View>
+      <View
+        style={{ backgroundColor: "yellowgreen", width: 100, height: 100 }}
+      />
+      <View style={{ backgroundColor: "blue", width: 100, height: 100 }}></View>
+      <View style={{ backgroundColor: "pink", width: 100, height: 100 }}></View>
+      <View
+        style={{ backgroundColor: "black", width: 100, height: 100 }}
+      ></View>
+      <View
+        style={{ backgroundColor: "purple", width: 100, height: 100 }}
       ></View>
     </View>
   );

a6128547a1153bce9da6082d3bce9c5cb23effb4
sia162 <nowitissiya@gmail.com>
2022-04-09 14:43:02 +0530

    b10: aligning items

--- App.js
+++ App.js
@@ -3,11 +3,26 @@ import { StyleSheet, SafeAreaView, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "grey", flex: 1, flexDirection: "row" }}>
+    <View
+      style={{
+        backgroundColor: "grey",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
       <View
         style={{ backgroundColor: "orange", width: 100, height: 100 }}
       ></View>
-      <View style={{ backgroundColor: "red", width: 100, height: 100 }}></View>
+      <View
+        style={{
+          backgroundColor: "red",
+          width: 100,
+          height: 200,
+          alignSelf: "flex-end",
+        }}
+      ></View>
       <View
         style={{ backgroundColor: "green", width: 100, height: 100 }}
       ></View>
